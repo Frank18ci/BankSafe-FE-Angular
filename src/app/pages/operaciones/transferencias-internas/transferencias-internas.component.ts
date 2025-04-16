9806ef4425801d43d3bfb1411e6f9d927b95f7e9
Frank <fcarpio113@gmail.com>
2025-04-16 09:30:59 -0500

    Version inicial

--- src/app/pages/operaciones/transferencias-internas/transferencias-internas.component.ts
+++ src/app/pages/operaciones/transferencias-internas/transferencias-internas.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 @Component({
   selector: 'app-transferencias-internas',
-  imports: [],
+  imports: [MdbFormsModule, ReactiveFormsModule],
   templateUrl: './transferencias-internas.component.html',
   styleUrl: './transferencias-internas.component.scss'
 })
