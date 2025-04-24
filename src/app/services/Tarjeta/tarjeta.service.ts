d54df0d82e8d071cd8b30dfb608a83e0ef07ce47
Frank <fcarpio113@gmail.com>
2025-04-24 14:00:06 -0500

    Agregando funcionalidad de solicitar tarjeta

--- src/app/services/Tarjeta/tarjeta.service.ts
+++ src/app/services/Tarjeta/tarjeta.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import Tarjeta from '../../model/Tarjeta';
 import { Page } from '../../model/Page';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class TarjetaService {
   private http = inject(HttpClient);
 	url = "http://localhost:8080";
   constructor() { }
-    save(tarjeta: Tarjeta){
+    save(tarjeta: Tarjeta): Observable<any>{
       return this.http.post(this.url + "/tarjeta", tarjeta)
     }
     findByNumeroTarjeta(numeroTarjeta: string){
